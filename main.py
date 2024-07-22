9e5c04ca7157d5341c619f4ae919d4d1feb5e060
rocio poppe <rocio.poppe@ioet.com>
2024-07-22 00:15:13 -0300

    adding variable for cors origins

--- main.py
+++ main.py
@@ -1,12 +1,15 @@
 from api import create_app
 from api.routers import hotels, reservations
 from fastapi.middleware.cors import CORSMiddleware
+from dotenv import load_dotenv
+import os
 
+load_dotenv()
 app = create_app()
 
 app.add_middleware(
     CORSMiddleware,
-    allow_origins= "http://localhost:3000",
+    allow_origins= os.getenv('FRONTEND_URL'),
     allow_credentials=True,
     allow_methods= ["GET", "POST", "OPTIONS", "PUT"],
     allow_headers=["*"],
